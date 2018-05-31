b3295ca141c4b9620a13a66991503c4f36dcc663
Jithu-geon <jithu.antony@gadgeon.com>
2018-05-31 09:32:54 +0530

    Some small changes

--- myAngularProj/Models/RegisterModel.cs
+++ myAngularProj/Models/RegisterModel.cs
@@ -2,8 +2,8 @@ namespace myAngularProj.Models
 {
     public class RegisterModel
     {
-        public string Name  ="ji";
-        public string email ="ranjitha" ;
+        public string Name  { get; set; }
+        public string email { get; set; }
         public string password { get; set; }
     }
 }
